ff629ccd87db6689b3ec0e479e43bfbe5e7ce91c
paulpatterson <Paul.Patterson@tirehub.com>
2024-03-12 21:21:07 -0500

    Added Data Folder with Category Example

---
+++ Data/ReactionsData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GoogleRareBe.Data
+{
+	public class ReactionsData
+	{
+		public ReactionsData()
+		{
+		}
+	}
+}
+
---
+++ Data/TagsData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GoogleRareBe.Data
+{
+	public class TagsData
+	{
+		public TagsData()
+		{
+		}
+	}
+}
+
