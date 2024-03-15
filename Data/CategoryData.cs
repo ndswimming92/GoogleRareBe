0d6e632a043b409080a0f457df2240b88932ae02
Lucas-Martin2023 <lucasmartin11990@gmail.com>
2024-03-14 20:18:48 -0500

    putting in Model info, Data info, and API info for Category

--- Data/CategoryData.cs
+++ Data/CategoryData.cs
@@ -6,8 +6,12 @@ namespace GoogleRareBe.Data
 	{
 		public static List<Category> Categories = new List<Category>
 		{
-			new Category {Id = 1, }
-		}
+			new Category {Id = 1, Label = "Sports" },
+            new Category {Id = 2, Label = "Food" },
+			new Category {Id = 3, Label = "Travel" },
+			new Category {Id = 4, Label = "Animals" },
+			new Category {Id = 5, Label = "News" }
+        };
 	}
 }
 
