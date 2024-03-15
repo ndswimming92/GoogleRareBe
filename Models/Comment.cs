0d084c4d500c36695fc9e76f77b2cf7d6400dccf
Austin Mangum <mangumaustin@gmail.com>
2024-03-14 19:58:19 -0500

    Comment API and DATA

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -2,5 +2,10 @@
 {
     public class Comment
     {
+        public int Id { get; set; }
+        public int Author_id { get; set; }
+        public int Post_id { get; set; }
+        public string Content { get; set; }
+        public DateTime Created_on { get; set; }
     }
 }
