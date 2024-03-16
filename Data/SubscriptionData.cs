43d7f82a9ae93b5eb4bb5a274f182e10734759c4
Lucas-Martin2023 <lucasmartin11990@gmail.com>
2024-03-16 09:54:42 -0500

    part of subscription info

--- Data/SubscriptionData.cs
+++ Data/SubscriptionData.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using GoogleRareBe.Models;
 namespace GoogleRareBe.Data
 {
-	public class SubscriptionData
-	{
-		public SubscriptionData()
-		{
-		}
-	}
+    public class SubscriptionData
+    {
+        public static List<Subscription> Categories = new List<Subscription>
+        {
+            new Subscription {Id = 1, Follower_id = 1, Author_id = 1, Created_on = DateTime.Now, Ended_on = DateTime.Now  },
+            new Subscription {Id = 2, Follower_id = 2, Author_id = 2, Created_on = DateTime.Now, Ended_on = DateTime.Now  },
+            new Subscription {Id = 3, Follower_id = 3, Author_id = 3, Created_on = DateTime.Now, Ended_on = DateTime.Now  },
+            new Subscription {Id = 4, Follower_id = 4, Author_id = 4, Created_on = DateTime.Now, Ended_on = DateTime.Now  },
+        };
+    }
 }
 
--- Models/Subscription.cs
+++ Models/Subscription.cs
@@ -2,5 +2,10 @@
 {
     public class Subscription
     {
+        public int Id { get; set; }
+        public int Follower_id { get; set; }
+        public int Author_id { get; set; }
+        public DateTime Created_on { get; set; }
+        public DateTime Ended_on { get; set; }
     }
 }
