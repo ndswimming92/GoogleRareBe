1793d5c4e34b33189bf4a850769454edd4df44ca
paulpatterson <Paul.Patterson@tirehub.com>
2024-03-16 10:49:29 -0500

    Added PostTags

---
+++ Data/PostTagData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using GoogleRareBe.Models;
+
+namespace GoogleRareBe.Data
+{
+	public class PostTagData
+	{
+		public static List<PostTag> PostTags = new List<PostTag>
+		{
+			new PostTag
+			{
+				Id = 1,
+				PostId = 1,
+				TagId = 1
+			},
+
+			new PostTag
+			{
+				Id = 2,
+				PostId = 2,
+				TagId = 2
+			},
+		};
+	}
+}
+
