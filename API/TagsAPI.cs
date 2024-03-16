6958a39119bb6543cfddd1b661b3f74b66477969
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-03-16 10:06:58 -0500

    Add a tag

--- API/TagsAPI.cs
+++ API/TagsAPI.cs
@@ -7,6 +7,15 @@ namespace GoogleRareBe.API
     {
         public static void Map(WebApplication app)
         {
+            // Endpoints
+
+            // Add Tag
+            app.MapPost("/api/addTag", (GoogleRareBeDbContext db, Tag addTag) =>
+            {
+                db.Tags.Add(addTag);
+                db.SaveChanges();
+                return Results.Ok();
+            });
 
         }
 
