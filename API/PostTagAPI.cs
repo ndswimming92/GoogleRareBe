bbdd8dbc19b64561a16936e8648e2e4dbc61248a
paulpatterson <Paul.Patterson@tirehub.com>
2024-03-23 09:50:15 -0500

    Added functionality for Post Tags

--- API/PostTagAPI.cs
+++ API/PostTagAPI.cs
@@ -37,6 +37,31 @@ namespace GoogleRareBe.API
                 }
             });
 
+            app.MapPatch("api/posts/{postId}/remove", (GoogleRareBeDbContext db, PostTagsDTO postTags) =>
+            {
+                Post post = db.Posts
+                            .Include(p => p.Tags)
+                            .SingleOrDefault(p => p.Id == postTags.PostId);
+                if (post == null)
+                {
+                    return Results.BadRequest("Invalid PostTagData");
+                }
+                if (post.Tags.Count < 1)
+                {
+                    return Results.BadRequest("No tags found");
+                }
+                Tag removeTag = post.Tags
+                                .SingleOrDefault(t => t.Id == postTags.TagId);
+                if (removeTag == null)
+                {
+                    return Results.BadRequest("Tag not Found");
+                }
+
+                post.Tags.Remove(removeTag);
+                db.SaveChanges();
+                return Results.Ok("tag removed");
+            });
+
         }
 
     }
